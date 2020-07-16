70d505d55d018bae3064e4e3f70c20e858950bde
VINOD KASHID <kashidvinod11@gmail.com>
2020-07-16 12:38:03 +0530

    commit test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
-
     public static void main(String[] args) {
+        System.out.println("Vinod demo test");
         SpringApplication.run(DemoApplication.class, args);
     }
 
