2bef0eb1b47e54959e2978bf0b54347fedcbb965
VINOD KASHID <kashidvinod11@gmail.com>
2020-07-16 13:08:27 +0530

    remove unused import

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
